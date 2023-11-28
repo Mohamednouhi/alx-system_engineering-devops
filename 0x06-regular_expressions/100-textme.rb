ac26295dc1965bcf24db9418ae0910edfe95d6db
mohamed nouhi <mohamednoouuhi@gmail.com>
2023-11-28 18:58:49 +0100

    advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,6 +3,6 @@
 
 from = ARGV[0].scan(/from:(.*?)\]/)
 to = ARGV[0].scan(/to:(.*?)\]/)
-flags = ARGV[0].scan(/flags:(.*?)/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
 
 puts [from, to, flags].join(',')
