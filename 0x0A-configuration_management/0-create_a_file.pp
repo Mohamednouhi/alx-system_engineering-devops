af793b59579ddd3dc04614884a5549fa6bef140e
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-01-19 11:13:01 -0800

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # 0-create_a_file.pp
 
 file { '/tmp/school':
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 
